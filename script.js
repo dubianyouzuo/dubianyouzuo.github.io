3f2322d519e8a09a42f16e90f94774b9b5480bce
dubianyouzuo <dubianyouzuo2@gmail.com>
2025-05-20 14:32:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const options = {
-  // Required: Your API key
-  key: 'BdTQkscvVZ1KTIKTJFAG9l9RgjZYf2lC',
 
-  // Optional: Initial state of the map
-  // 名古屋駅を中心に表示
-  lat: 35.170736,
-  lon: 136.882104,
-  zoom: 6,
-};
-
-// Initialize Windy API
-windyInit(options, windyAPI => {
-    const { map } = windyAPI;
-    // .map is instance of Leaflet map
-});
